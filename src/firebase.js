19b52a87054d3acdd780e4b4082b373be126dc75
basilfaheem <basil.faheem25@gmail.com>
2020-12-20 12:09:15 +0530

    second commmit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBGkTj2JrgRENcvLS-aN3GROoWARQCzI84",
-    authDomain: "clone-9bd63.firebaseapp.com",
-    projectId: "clone-9bd63",
-    storageBucket: "clone-9bd63.appspot.com",
-    messagingSenderId: "798851394572",
-    appId: "1:798851394572:web:bde1a0ce82644524c81229",
-    measurementId: "G-NMBCGLN87S"
+    // paste your firebaseconfig here.
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
